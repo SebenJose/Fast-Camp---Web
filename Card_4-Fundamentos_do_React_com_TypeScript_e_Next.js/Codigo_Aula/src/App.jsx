66b666406f7516a59a240d00d0d4942eecc661d4
SebenJose <josecarlospc001@gmail.com>
2026-02-10 15:39:59 -0300

    Iniciando com React

---
+++ Card_4-Fundamentos_do_React_com_TypeScript_e_Next.js/Codigo_Aula/src/App.jsx
@@ -0,0 +1,25 @@
+/*
+import { useState } from "react";
+
+codigo comentado pois no video foi removido para desenvolvimento do prototipo de tela
+mas achei interessante deixar aqui para mostrar o uso do useState e fixar o exemplo
+
+function App() {
+  const [message, setMessage] = useState("Olá, mundo!");
+
+  return (
+    <div>
+      <h1>{message}</h1>
+      <button
+        onClick={() => {
+          setMessage("Olá, fui clicado!");
+        }}
+      >
+        Mudar mensagem
+      </button>
+    </div>
+  );
+}
+
+export default App;
+*/
