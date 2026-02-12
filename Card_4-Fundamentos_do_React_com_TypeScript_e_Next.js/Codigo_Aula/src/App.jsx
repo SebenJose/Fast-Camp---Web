c3b7df0433dd2a499a6b84e74e5b8eca10abc742
SebenJose <josecarlospc001@gmail.com>
2026-02-12 16:32:49 -0300

    ,,,

--- Card_4-Fundamentos_do_React_com_TypeScript_e_Next.js/Codigo_Aula/src/App.jsx
+++ Card_4-Fundamentos_do_React_com_TypeScript_e_Next.js/Codigo_Aula/src/App.jsx
@@ -27,6 +27,7 @@ export default App;
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 import { useState } from "react";
+import {v4} from "uuid";
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -69,14 +70,28 @@ function App() {
     setTasks(newTasks);
   }
 
+  function onAddTaskSubmit(title, description) {
+    const newTask = {
+      id: v4(),
+      title,
+      description,
+      isCompleted: false,
+    };
+    setTasks([...tasks, newTask]);
+  }
+
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
-      <div className="w-[500px]">
+      <div className="w-[500px] space-y-4">
         <h1 className="text-3xl text-slate-100 font-bold text-center">
           Gerenciador de Tarefas
         </h1>
-        <AddTask />
-        <Tasks tasks={tasks} onTaskClick={onTaskClick} onDeleteTaskClick={onDeleteTaskClick} />
+        <AddTask onAddTaskSubmit={onAddTaskSubmit} />
+        <Tasks
+          tasks={tasks}
+          onTaskClick={onTaskClick}
+          onDeleteTaskClick={onDeleteTaskClick}
+        />
       </div>
     </div>
   );
