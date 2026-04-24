39a283084cbdcf90484a9eb69133a68083ba5632
SebenJose <josecarlospc001@gmail.com>
2026-04-24 20:10:10 -0300

    feat: implement user management with CRUD operations and database integration

--- Card_7_-Integracao_com_Banco_de_Dados_usando_FastAPI/Codigos_Aula/fast_zero/fast_zero/app.py
+++ Card_7_-Integracao_com_Banco_de_Dados_usando_FastAPI/Codigos_Aula/fast_zero/fast_zero/app.py
@@ -1,11 +1,14 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy import or_, select
+from sqlalchemy.orm import Session
 
-from fast_zero.schemas import Message, UserDB, UserList, UserPublic, UserSchema
+from fast_zero.database import get_session
+from fast_zero.models import User
+from fast_zero.schemas import Message, UserList, UserPublic, UserSchema
 
 app = FastAPI(title='Minha API')
-database = []
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
@@ -14,40 +17,83 @@ def read_root():
 
 
 @app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
-def create_user(user: UserSchema):
-    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+def create_user(user: UserSchema, session=Depends(get_session)):
 
-    database.append(user_with_id)
+    db_user = session.scalar(
+        select(User).where(
+            or_(User.username == user.username, User.email == user.email)
+        )
+    )
 
-    return user_with_id
+    if db_user:
+        if db_user.username == user.username:
+            raise HTTPException(
+                status_code=HTTPStatus.BAD_REQUEST,
+                detail='Username already exists!',
+            )
+        elif db_user.email == user.email:
+            raise HTTPException(
+                status_code=HTTPStatus.BAD_REQUEST,
+                detail='Email already exists!',
+            )
+
+    db_user = User(
+        username=user.username, email=user.email, password=user.password
+    )
+
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
+
+    return db_user
 
 
 @app.get('/users/', status_code=HTTPStatus.OK, response_model=UserList)
-def read_users():
-    return {'users': database}
+def read_users(session: Session = Depends(get_session)):
+
+    user = session.scalars(select(User))
+
+    return {'users': user}
 
 
 @app.put(
     '/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic
 )
-def update_user(user_id: int, user: UserSchema):
-    user_with_id = UserDB(**user.model_dump(), id=user_id)
+def update_user(
+    user_id: int, user: UserSchema, session: Session = Depends(get_session)
+):
 
-    if user_id > 1 or user_id > len(database):
+    db_user = session.scalar(select(User).where(User.id == user_id))
+
+    if not db_user:
         raise HTTPException(
             status_code=HTTPStatus.NOT_FOUND, detail='User not found!'
         )
 
-    database[user_id - 1] = user_with_id
-    return user_with_id
+    db_user.email = user.email
+    db_user.username = user.username
+    db_user.password = user.password
+
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
+
+    return db_user
 
 
 @app.delete(
     '/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic
 )
-def delete_user(user_id: int):
-    if user_id > 1 or user_id > len(database):
+def delete_user(user_id: int, session: Session = Depends(get_session)):
+
+    db_user = session.scalar(select(User).where(User.id == user_id))
+
+    if not db_user:
         raise HTTPException(
             status_code=HTTPStatus.NOT_FOUND, detail='User not found!'
         )
-    return database.pop(user_id - 1)
+
+    session.delete(db_user)
+    session.commit()
+
+    return db_user
--- Card_7_-Integracao_com_Banco_de_Dados_usando_FastAPI/Codigos_Aula/fast_zero/tests/test_app.py
+++ Card_7_-Integracao_com_Banco_de_Dados_usando_FastAPI/Codigos_Aula/fast_zero/tests/test_app.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
 
+from fast_zero.schemas import UserPublic
+
 
 def test_root_deve_retornar_ok_e_ola_mundo(client):
 
@@ -9,7 +11,7 @@ def test_root_deve_retornar_ok_e_ola_mundo(client):
     assert response.json() == {'message': 'Ola Mundo!'}
 
 
-def test_crate_user(client):
+def test_create_user(client):
 
     response = client.post(
         '/users/',
@@ -32,18 +34,18 @@ def test_read_users(client):
     response = client.get('/users')
 
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {
-        'users': [
-            {
-                'id': 1,
-                'email': 'alice@example.com',
-                'username': 'alice',
-            },
-        ]
-    }
+    assert response.json() == {'users': []}
 
 
-def test_update_user(client):
+def test_read_users_with_users(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get('/users')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'users': [user_schema]}
+
+
+def test_update_user(client, user):
     response = client.put(
         '/users/1',
         json={
@@ -61,12 +63,12 @@ def test_update_user(client):
     }
 
 
-def test_delete_user(client):
+def test_delete_user(client, user):
     response = client.delete('/users/1')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {
-        'username': 'bob',
-        'email': 'bob@example.com',
+        'username': 'Teste',
+        'email': 'teste@example.com',
         'id': 1,
     }
